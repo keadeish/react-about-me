4452e11a1d26bde6f0745f5c9ebdb2fab0b861c1
Keadeish <keadeish10@hotmail.com>
2022-07-18 10:07:47 +0100

    added changes

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -4,7 +4,7 @@ function PageHeader(): JSX.Element {
       <h1>Ye Olde Web Page</h1>
       <p>Welcome to Nichard's Ye Olde Web Page!</p>
       <p>
-        This shows a random scatter of my thoughts, last updated on March 4,
+        This shows. a random scatter of my thoughts, last updated on March 4,
         2003.
       </p>
     </header>
